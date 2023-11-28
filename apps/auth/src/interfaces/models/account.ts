1e413254485220b12b322644f452d67700a7d17a
nguyenson <nson19072001@gmail.com>
2023-11-28 11:01:12 +0700

    chinh sua file account

--- apps/auth/src/interfaces/models/account.ts
+++ apps/auth/src/interfaces/models/account.ts
@@ -11,3 +11,16 @@ export interface IAccount {
     is_active: boolean;
     roles: string[];
 }
+//bonus the following code
+export interface Account{
+    id: string;
+    email: string;
+    password?: string;
+    updated_time: Date;
+    created_time: Date;
+    last_locked?: Date;
+    failed_login: number;
+    password_token?: string;
+    token_time?: Date;
+    is_active: boolean;
+}
